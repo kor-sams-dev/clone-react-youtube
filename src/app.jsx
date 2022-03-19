ede22ea1038b89fd6734d141603bb53c6bacc5f9
shs <godppun@naver.com>
2022-03-19 19:27:32 +0900

    create youtube clone code and make video list useing youtube open apis

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import VideoList from "./components/video_list/video_list";
 
-import './App.css';
+const API_KEY = "AIzaSyA2Jcbw3HbA3HyrguMrKSDEe4jpnO9y404";
 
 function App() {
-  return <h1>Hello</h1>
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${API_KEY}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  useEffect(() => {
+    console.log(videos);
+  }, [videos]);
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
